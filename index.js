7edd4301006407cfa8235b6d692c4ef26f433591
tiwariparv <tiwari.parv@gmail.com>
2020-09-25 05:11:59 +0530

    first draft

---
+++ index.js
@@ -0,0 +1,22 @@
+const typeFunctions = require('./dependencies/typeFunctions');
+const structDiff = require('./dependencies/structDiff');
+const structPatch = require('./dependencies/structPatch');
+
+
+var VersionManagement = (function() {
+    function VersionManagement() {}
+
+    VersionManagement.prototype.structDiff = function(orignalStruct, changedStruct) {
+        const diff = new structDiff(orignalStruct, changedStruct);
+        return diff.getStructDiff();
+    };
+
+    VersionManagement.prototype.patchDiff = function(changedStruct, changes) {
+        const diff = new structPatch(changedStruct, changes);
+        return diff.getStructPatch();
+    };
+
+    return VersionManagement;
+})();
+
+module.exports = VersionManagement;
